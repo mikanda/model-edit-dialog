295dbcd80fdb6da3462ab63e56cbccf2bb63a892
Lukas <github@luka5.de>
2013-05-24 12:20:49 +0200

    using yes-no-dialog now

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
  * require dependencies
  */
 var Form = require('forms'),
-    dialog = require('bootstrap-dialog'),
-    domify = require('domify'),
-    event = require('event');
+    yesNoDialog = require('yes-no-dialog');
+
 /*
  * create new instance
  */
 function ModelEditDialog (model, formschema, lang) {
+  var title;
   /*
    * set default lang if not definied
    */
@@ -18,60 +18,45 @@ function ModelEditDialog (model, formschema, lang) {
     lang.save = 'save';
   if (lang.cancel === undefined)
     lang.cancel = 'cancel';
-  if (lang.title === undefined)
-    lang.title = 'Edit model';
+
+  title = lang.title;
+  if (title === undefined)
+    title = 'Edit model';
+  else
+    delete lang.title;
   
   /*
    * create new form
    */
-  var form = new Form(formschema);
+  var form = this.form = new Form(formschema);
   form.render();
   form.setModel(model);
 
   /*
-   * set footer
-   */
-  var saveEl = domify('<button class="btn btn-primary">' + lang.save + '</button>')[0];
-  var cancelEl = domify('<button class="btn">' + lang.cancel + '</button>')[0];
-  var footEl = domify('<span></span>')[0];
-  footEl.appendChild(cancelEl);
-  footEl.appendChild(saveEl);
-  
-  /*
    * make new bootstrap-dialog
    */
-  var d = dialog(lang.title, form.view, footEl)
-    .closable()
-    .overlay()
-    .movable()
-    .show();
-
-  /*
-   * bind button events and implement standard behaviour
-   */
-  event.bind(saveEl, 'click', 
-    function () {
-      /*
-       * hide dialog
-       * update and save model
-       */
-      d.hide();
-      var model = form.getModel();
-      model.save(function (err) {
-        console.log(err);
-      });
-    }
-  );
-  event.bind(cancelEl, 'click', 
-    function () {
-      /*
-       * hide dialog
-       */
-      d.hide();
-    }
+  yesNoDialog(
+    title,
+    form.view,
+    {
+      fn: this.save,
+      scope: this
+    },
+    {
+      fn: this.cancel,
+      scope: this
+    },
+    lang
   );
 };
-
+ModelEditDialog.prototype.save = function () {
+  var model = this.form.getModel();
+  model.save(function (err) {
+    console.log(err);
+  });
+};
+ModelEditDialog.prototype.cancel = function () {
+};
 module.exports = function (model, formschema, lang) {
   return new ModelEditDialog(model, formschema, lang);
 };
